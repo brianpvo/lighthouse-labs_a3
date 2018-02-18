40f40a0cd8ad3cad01a9e150e5e4f4fc16de1be4
Brian Vo <brian.p.vo@hotmail.com>
2018-02-18 11:33:18 -0800

    Done Assignment 3

---
+++ main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  LHL_a3
+//
+//  Created by Brian Vo on 2018-02-18.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSNumber *findMax(NSArray *array) {
+    NSNumber *max = array[0];
+    
+    for (int i = 0; i < [array count]; i++) {
+        
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSArray *array = @[@3, @7, @6, @8];
+        NSArray *array2 = @[@44, @5, @6];
+        
+        NSLog(@"%@", findMax(array));
+        NSLog(@"%@", findMax(array2));
+        
+    }
+    return 0;
+}
